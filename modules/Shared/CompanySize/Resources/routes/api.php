09b6ecad5eba6284cba8e421ccac1e18bf0ef98d
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-04-25 06:01:22 +0300

    add register comapny

--- modules/Shared/CompanySize/Resources/routes/api.php
+++ modules/Shared/CompanySize/Resources/routes/api.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Shared\CompanySize\Controllers\CompanySizeController;
 
-Route::group(['middleware' => ['auth:api']], function () {
+// Route::group(['middleware' => ['auth:api']], function () {
     Route::get('/', [CompanySizeController::class, 'index']);
-    Route::post('/', [CompanySizeController::class, 'store']);
-    Route::get('/{id}', [CompanySizeController::class, 'show']);
-    Route::put('/{id}', [CompanySizeController::class, 'update']);
-    Route::delete('/{id}', [CompanySizeController::class, 'delete']);
-});
+//     Route::post('/', [CompanySizeController::class, 'store']);
+//     Route::get('/{id}', [CompanySizeController::class, 'show']);
+//     Route::put('/{id}', [CompanySizeController::class, 'update']);
+//     Route::delete('/{id}', [CompanySizeController::class, 'delete']);
+// });
