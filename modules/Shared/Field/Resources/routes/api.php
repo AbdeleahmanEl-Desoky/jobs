9d87ca860481a3762c72319520a84cf38e8e51c1
Abdelrahman El-Desoky <dev.deesoky@gmail.com>
2025-04-25 06:00:03 +0300

    add register comapny

--- modules/Shared/Field/Resources/routes/api.php
+++ modules/Shared/Field/Resources/routes/api.php
@@ -3,10 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Shared\Field\Controllers\FieldController;
 
-Route::group(['middleware' => ['auth:api']], function () {
     Route::get('/', [FieldController::class, 'index']);
-    Route::post('/', [FieldController::class, 'store']);
-    Route::get('/{id}', [FieldController::class, 'show']);
-    Route::put('/{id}', [FieldController::class, 'update']);
-    Route::delete('/{id}', [FieldController::class, 'delete']);
-});
+
